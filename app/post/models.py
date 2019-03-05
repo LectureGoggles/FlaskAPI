147c6a3d8eae20932378d097a208a8cb29e5f222
Zachary Johnson <zackjohnson8@gmail.com>
2019-03-04 20:34:43 -0800

    Fixed a model so that it works with the db for now

--- app/post/models.py
+++ app/post/models.py
@@ -7,7 +7,7 @@ class Subject(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     subject = db.Column(db.String(50), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer) # TODO(zack): , db.ForeignKey('user.id')
     description = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=dt.datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
@@ -18,7 +18,7 @@ class Resource(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     subject = db.Column(db.String(50), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer) # TODO(zack): , db.ForeignKey('user.id')
     author = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     upvote = db.Column(db.Integer, default=0)
