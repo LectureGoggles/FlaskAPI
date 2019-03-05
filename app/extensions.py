7d43101f4d67ef48e42c56a11cd4b3e8a7e6ac55
Zachary Johnson <zackjohnson8@gmail.com>
2019-03-04 19:49:34 -0800

    Added: login responds with user token to be used for complete session. Logout is added but maybe removed later. Passwords are encrypted using bcrypt to authenticate users and security use.

--- app/extensions.py
+++ app/extensions.py
@@ -3,10 +3,14 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
 cors = CORS()
-jwt = JWTManager() #TODO(zack): do the work
+jwt = JWTManager()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
--- app/user/views.py
+++ app/user/views.py
@@ -2,21 +2,19 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, jwt_optional, create_access_token, current_user
 from flask import jsonify, json, render_template, flash, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
 
-from app.extensions import db
+from app.extensions import db, login_manager, bcrypt
 from .forms import RegisterForm, LoginForm
 from .models import User
 
 blueprint = Blueprint('user', __name__)
 
 @blueprint.route('/users/signup', methods=('POST',))
-def register_user():
+def _register_user():
 
     form = RegisterForm()
-    
-    user = User(username=form.username.data, email=form.email.data, password=form.password.data, school=form.school.data)
-    #user.token = create_access_token(identity=user) TODO: ADD TOKEN
-
+    user = User(username=form.username.data, email=form.email.data, password=bcrypt.generate_password_hash(form.password.data), school=form.school.data)
     db.session.add(user)
     db.session.commit()
     return jsonify("true")
@@ -24,10 +22,32 @@ def register_user():
     #return json.dumps({'success':False}), 200, {'ContentType':'application/json'}
 
 @blueprint.route('/users/login', methods=('POST',))
-def login_user():
+def _login_user():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-    return json.dumps({'success':False}), 500, {'ContentType':'application/json'} 
+    print(user.email)
+    if user:
+        if bcrypt.check_password_hash(user.password, form.password.data):
+            # authenticate user and resave into db
+            user.authenticate = True
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            flash('Login requested for user {}'.format(user.email))
+            access_token = create_access_token(identity=user.username) #Create access token for user
+            return jsonify(access_token=access_token), 200
+        
+    return json.dumps({'Login':False}), 500, {'ContentType':'application/json'} 
+
+@blueprint.route("/users/logout", methods=["GET"])
+@login_required
+def logout():
+    """Logout the current user."""
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(email=user_id).first()
