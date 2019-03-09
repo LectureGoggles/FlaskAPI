36a0971e51b3b71024d003e9753d23808d1fd563
Zachary Johnson <zackjohnson8@gmail.com>
2019-03-09 14:43:09 -0800

    Finished: subject create api call

--- app/post/views.py
+++ app/post/views.py
@@ -12,8 +12,22 @@ blueprint = Blueprint('post', __name__)
 @jwt_required
 def _subjectcreate():
     form = SubjectCreation()
-
     current_user = get_jwt_identity()
 
+    # check for subject already created
+    duplicatesubject = Subject.query.filter_by(subject=form.subject.data).first()
+    if duplicatesubject:
+        return jsonify(success='False', code=400, description='duplicate subject')
+
+    # Check to see if subject exists
+    if current_user:
+        user = User.query.filter_by(username=current_user).first()
+        subject = Subject(
+            subject=form.subject.data,
+            description=form.description.data,
+            author_id=user.id
+        )
     
+    return jsonify(success='False', code=400, description='invalid token')
+
     # TODO(zack): implement
--- app/user/views.py
+++ app/user/views.py
@@ -10,23 +10,14 @@ from .models import User
 
 blueprint = Blueprint('user', __name__)
 
-
 @blueprint.route('/users/signup', methods=('POST', ))
 def _register_user():
 
     duplicateuser = User.query.filter_by(email=form.email.data).first()
     if duplicateuser:
-        return jsonify(status=fail, code=400)
+        return jsonify(success='False', code=400)
 
     form = RegisterForm()
-<<<<<<< HEAD
-    user = User(username=form.username.data, email=form.email.data, password=bcrypt.generate_password_hash(form.password.data), school=form.school.data)
-    
-    if user:
-        db.session.add(user)
-        db.session.commit()
-        return json.dumps({'success':True}), 201
-=======
     user = User(
         username=form.username.data,
         email=form.email.data,
@@ -36,9 +27,6 @@ def _register_user():
     db.session.commit()
     return jsonify("true")
 
-    # return json.dumps({'success':False}), 200, {'ContentType':'application/json'}
-
->>>>>>> f0ba98baf5f98a74cd62c9a7605131653344ff33
 
 @blueprint.route('/users/login', methods=('POST', ))
 def _login_user():
@@ -50,34 +38,10 @@ def _login_user():
             db.session.add(user)
             db.session.commit()
             flash('Login requested for user {}'.format(user.email))
-<<<<<<< HEAD
             access_token = create_access_token(identity=user.username) #Create access token for user
-            return jsonify(access_token=access_token, success=True, response=200)
+            return jsonify(access_token=access_token, success='True', response=200)
         
     return json.dumps({'Login':False}), 400
-
-
-
-=======
-            access_token = create_access_token(
-                identity=user.username)  # Create access token for user
-            return jsonify(access_token=access_token), 200
-
-    return json.dumps({'Login': False}), 500, {
-        'ContentType': 'application/json'
-    }
-
-
-@blueprint.route("/users/logout", methods=["GET"])
-def _logout():
-    """Logout the current user."""
-    user = current_user
-    user.authenticated = False
-    db.session.add(user)
-    db.session.commit()
-    logout_user()
->>>>>>> f0ba98baf5f98a74cd62c9a7605131653344ff33
-
 
 @blueprint.route("/users/auth", methods=["GET"])
 @jwt_optional
