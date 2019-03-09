bf36c1ae6cb241b5e20870f3e32b0e2304e39e7f
Zachary Johnson <zackjohnson8@gmail.com>
2019-03-09 15:48:54 -0800

    Using a bearer token on postman, the create subject now works. You can search and verify this using the _getsubject directory with an included name.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from app.user.models import User
 from app import user, post
 
 POSTGRES = {
-    'user': 'nyocum',
+    'user': 'zack',
     'pw': 'password',
     'db': 'mydb1',
     'host': 'localhost',
