dfb0020170bc8acd3c31a42eb924aa3f53ee0d85
Logan <loganjohnlong@users.noreply.github.com>
2020-02-12 17:31:43 -0600

    Fix App Insights not getting logs

--- autoapp.py
+++ autoapp.py
@@ -1,15 +1,20 @@
 from app.__init__ import create_app
+from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace import config_integration
+import os
 
 app = create_app()
-middleware = FlaskMiddleware(app)
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string=os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]),
+    sampler=ProbabilitySampler(rate=1.0)
+)
 config_integration.trace_integrations(['sqlalchemy'])
 
 if __name__ == "__main__":
     import logging
     logger = logging.getLogger('werkzeug')
-    logger.addHandler(AzureLogHandler())
     logger.setLevel(logging.ERROR)
     app.run(host='0.0.0.0', threaded=True)
