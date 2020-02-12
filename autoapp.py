d20a383fd409ddd464de406ac2e0866f92d85645
Logan Long <loganjohnlong@users.noreply.github.com>
2020-02-12 12:54:15 -0600

    Add OpenCensus Flask and Azure extensions

--- autoapp.py
+++ autoapp.py
@@ -1,6 +1,15 @@
 from app.__init__ import create_app
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 app = create_app()
+middleware = FlaskMiddleware(
+    app
+)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    import logging
+    logger = logging.getLogger('werkzeug')
+    logger.addHandler(AzureLogHandler())
+    logger.setLevel(logging.ERROR)
+    app.run(host='0.0.0.0', threaded=True)
