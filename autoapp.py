9480d0acfec905d6d3c2cae2b41acbcc604d745d
Logan Long <loganjohnlong@users.noreply.github.com>
2020-02-12 15:51:20 -0600

    Add OpenCensus SQLAlchemy extension

--- autoapp.py
+++ autoapp.py
@@ -1,11 +1,11 @@
 from app.__init__ import create_app
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.trace import config_integration
 
 app = create_app()
-middleware = FlaskMiddleware(
-    app
-)
+middleware = FlaskMiddleware(app)
+config_integration.trace_integrations(['sqlalchemy'])
 
 if __name__ == "__main__":
     import logging
