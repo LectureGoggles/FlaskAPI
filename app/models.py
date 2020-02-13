80e2748a57217348a68aed60c2922a93e9ceae14
Logan <loganjohnlong@users.noreply.github.com>
2020-02-12 19:04:41 -0600

    Update models.py
    Fix Codacy issues

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class BaseModel(db.Model):
 
     def __repr__(self):
         """Define a base way to print models."""
+
         return '%s(%s)' % (self.__class__.__name__, {
             column: value
             for column, value in self._to_dict().items()
@@ -18,9 +19,8 @@ class BaseModel(db.Model):
 
     def json(self):
         """Define a base way to jsonify models, dealing with datetime objects."""
+
         return {
             column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
             for column, value in self._to_dict().items()
         }
-
-
